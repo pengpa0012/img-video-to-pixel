7ff659901fe6d00808462d5f42647f6c85566caf
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-22 06:58:28 +0800

    setup p5 js, added img upload display on canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,39 @@
 // TODO:
-// - setup p5 js
-// - create ui (file upload - (img/video) / record via camera)
+// - create ui (file upload - (video) / record via camera)
 // - display on canvas
 // - get image data
 // - get coordinates and paint on canvas (pixelated)
+let img
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+function draw() {
+  if(img) image(img, 0, 0, windowWidth, windowHeight)
+}
+
+
+// UI
+const fileInput = document.querySelector(".file-input")
+const resetBtn = document.querySelector(".reset")
+
+fileInput.addEventListener("change", displayImg)
+resetBtn.addEventListener("click", () => {
+  img = undefined
+  fileInput.value = ""
+  resetBtn.classList.add("hide")
+  fileInput.classList.remove("hide")
+})
+
+function displayImg(e) {
+  const myImageFile = e.target.files[0]
+  let urlOfImageFile = URL.createObjectURL(myImageFile)
+  img = loadImage(urlOfImageFile, () => {image(img, 0, 0)})
+  fileInput.classList.add("hide")
+  resetBtn.classList.remove("hide")
+}
