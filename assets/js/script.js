95e14f64a452d6acf040e41f35eddbf92379afe5
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-22 07:51:08 +0800

    added img pixel

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,24 @@
 // - display on canvas
 // - get image data
 // - get coordinates and paint on canvas (pixelated)
-let img
+let img, pixelation_level = 10
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(500, 500)
 }
 
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
-}
-
-function draw() {
-  if(img) image(img, 0, 0, windowWidth, windowHeight)
-}
+// use draw on video/record maybe
+// function draw() {
+//   if(img) {
+//     img.loadPixels()
+//     console.log(img.pixels)
+    
+//     // Update the image.
+//     // img.updatePixels();
+  
+//     // // Display the image.
+//     // image(img, 0, 0, 500, 500);
+//   }
+// }
 
 
 // UI
@@ -28,12 +33,36 @@ resetBtn.addEventListener("click", () => {
   fileInput.value = ""
   resetBtn.classList.add("hide")
   fileInput.classList.remove("hide")
+  clear()
 })
 
 function displayImg(e) {
   const myImageFile = e.target.files[0]
   let urlOfImageFile = URL.createObjectURL(myImageFile)
-  img = loadImage(urlOfImageFile, () => {image(img, 0, 0)})
+  img = loadImage(urlOfImageFile, () => pixelizeImg(img, pixelation_level))
   fileInput.classList.add("hide")
   resetBtn.classList.remove("hide")
+}
+
+function pixelizeImg(img, pixelation_level) {
+  pixelDensity(1)
+  image(img, 0, 0, width, height)
+  loadPixels()
+  noStroke()
+  for (let x = 0; x < width; x += pixelation_level) {
+    for (let y = 0; y < height; y += pixelation_level) {
+      let i = (x + y * width) * 4
+
+      let r = pixels[i + 0]
+      let g = pixels[i + 1]
+      let b = pixels[i + 2]
+      let a = pixels[i + 3]
+
+      // add delay on pixelization
+      setTimeout(() => {
+        fill(r, g, b, a)
+        rect(x, y, 10, 10)
+      }, y * 5)
+    }
+  }
 }
