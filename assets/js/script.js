6f2b8ace4286b1c57a992ce1409ec46d606b36bb
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-21 22:35:51 +0800

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// TODO:
+// - setup p5 js
+// - create ui (file upload - (img/video) / record via camera)
+// - display on canvas
+// - get image data
+// - get coordinates and paint on canvas (pixelated)
