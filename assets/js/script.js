26744815c8adef95a63b5bc97092dd8d3b9fe1aa
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-22 12:32:42 +0800

    added video capture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,42 @@
 // TODO:
-// - create ui (file upload - (video) / record via camera)
-// - display on canvas
-// - get image data
-// - get coordinates and paint on canvas (pixelated)
-let img, pixelation_level = 5, threshold = 128
+// - create ui (file upload - (video))
+// - reprompt camera permission
+
+let img, capture, pixelation_level = 5, threshold = 128, showCapture = false
 function setup() {
   createCanvas(500, 500)
 }
 
-// use draw on video/record maybe
-// function draw() {
-//   if(img) {
-//     img.loadPixels()
-//     console.log(img.pixels)
-    
-//     // Update the image.
-//     // img.updatePixels();
-  
-//     // // Display the image.
-//     // image(img, 0, 0, 500, 500);
-//   }
-// }
+function draw() {
+  if(showCapture) {
+    pixelizeImg(capture, pixelation_level, true)
+    console.log(capture)
+  }
+}
 
 
 // UI
 const fileInput = document.querySelector(".file-input")
 const resetBtn = document.querySelector(".reset")
+const cameraBtn = document.querySelector(".camera")
 
 fileInput.addEventListener("change", displayImg)
 resetBtn.addEventListener("click", () => {
   img = undefined
+  showCapture = false 
   fileInput.value = ""
   resetBtn.classList.add("hide")
   fileInput.classList.remove("hide")
+  cameraBtn.classList.remove("hide")
   clear()
+})
+cameraBtn.addEventListener("click", () => {
+  fileInput.classList.add("hide")
+  cameraBtn.classList.add("hide")
+  resetBtn.classList.remove("hide")
+  capture = createCapture(VIDEO)
+  capture.hide()
+  showCapture = true  
 })
 
 function displayImg(e) {
@@ -41,22 +44,15 @@ function displayImg(e) {
   let urlOfImageFile = URL.createObjectURL(myImageFile)
   img = loadImage(urlOfImageFile, () => pixelizeImg(img, pixelation_level))
   fileInput.classList.add("hide")
+  cameraBtn.classList.add("hide")
   setTimeout(() => {
     resetBtn.classList.remove("hide")
   }, 5000)
 }
 
-function pixelizeImg(img, pixelation_level) {
+function pixelizeImg(img, pixelation_level, isVideo) {
   pixelDensity(1)
-  // adjust image size to fit to canvas (object-fit: cover)
-  const scaleX = 500 / img.width
-  const scaleY = 500 / img.height
-  const scale = Math.max(scaleX, scaleY)
-  const newWidth = img.width * scale
-  const newHeight = img.height * scale
-  const offsetX = (500 - newWidth) / 2
-  const offsetY = (500 - newHeight) / 2
-
+  const {offsetX, offsetY, newWidth, newHeight} = fixedResolution(500, img)
   image(img, offsetX, offsetY, newWidth, newHeight)
   loadPixels()
   noStroke()
@@ -74,11 +70,29 @@ function pixelizeImg(img, pixelation_level) {
       // Determine color based on brightness and threshold
       let color = brightness > threshold ? 255 : 0
 
-      // add delay on pixelization
-      setTimeout(() => {
+      if(!isVideo) {
+        // add delay on pixelization
+        setTimeout(() => {
+          fill(`rgb(${color}, ${color}, ${color})`)
+          rect(x, y, 50, 50)
+        }, y * 5)
+      } else {
         fill(`rgb(${color}, ${color}, ${color})`)
         rect(x, y, 50, 50)
-      }, y * 5)
+      }
     }
   }
+}
+
+function fixedResolution(width, element) {
+  // adjust image size to fit to canvas (object-fit: cover)
+  const scaleX = width / element.width
+  const scaleY = width / element.height
+  const scale = Math.max(scaleX, scaleY)
+  const newWidth = element.width * scale
+  const newHeight = element.height * scale
+  const offsetX = (width - newWidth) / 2
+  const offsetY = (width - newHeight) / 2
+
+  return {offsetX, offsetY, newWidth, newHeight}
 }
