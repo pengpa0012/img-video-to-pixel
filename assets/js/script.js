ed4c693054c663e5cb671b880356a6c2b8151a0c
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-22 08:41:13 +0800

    update pixel to black and white, adjust image scale

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // - display on canvas
 // - get image data
 // - get coordinates and paint on canvas (pixelated)
-let img, pixelation_level = 10
+let img, pixelation_level = 5, threshold = 128
 function setup() {
   createCanvas(500, 500)
 }
@@ -41,12 +41,23 @@ function displayImg(e) {
   let urlOfImageFile = URL.createObjectURL(myImageFile)
   img = loadImage(urlOfImageFile, () => pixelizeImg(img, pixelation_level))
   fileInput.classList.add("hide")
-  resetBtn.classList.remove("hide")
+  setTimeout(() => {
+    resetBtn.classList.remove("hide")
+  }, 5000)
 }
 
 function pixelizeImg(img, pixelation_level) {
   pixelDensity(1)
-  image(img, 0, 0, width, height)
+  // adjust image size to fit to canvas (object-fit: cover)
+  const scaleX = 500 / img.width
+  const scaleY = 500 / img.height
+  const scale = Math.max(scaleX, scaleY)
+  const newWidth = img.width * scale
+  const newHeight = img.height * scale
+  const offsetX = (500 - newWidth) / 2
+  const offsetY = (500 - newHeight) / 2
+
+  image(img, offsetX, offsetY, newWidth, newHeight)
   loadPixels()
   noStroke()
   for (let x = 0; x < width; x += pixelation_level) {
@@ -58,10 +69,15 @@ function pixelizeImg(img, pixelation_level) {
       let b = pixels[i + 2]
       let a = pixels[i + 3]
 
+      // Calculate brightness
+      let brightness = 0.299 * r + 0.587 * g + 0.114 * b
+      // Determine color based on brightness and threshold
+      let color = brightness > threshold ? 255 : 0
+
       // add delay on pixelization
       setTimeout(() => {
-        fill(r, g, b, a)
-        rect(x, y, 10, 10)
+        fill(`rgb(${color}, ${color}, ${color})`)
+        rect(x, y, 50, 50)
       }, y * 5)
     }
   }
